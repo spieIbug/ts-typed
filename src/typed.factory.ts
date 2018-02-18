5b9b4cc90a9264b3e73e46d5a0eccb0f7c075f30
ymeddah <my.meddah@gmail.com>
2018-02-18 15:52:54 +0100

    feature(typed): TypedFactory.create(data, model)

---
+++ src/typed.factory.ts
@@ -0,0 +1,25 @@
+import {Type} from '@angular/core';
+
+/**
+ * This factory aims to create a typed object for a given data (Array, or object), and model
+ *
+ * @author: Maamar Yacine MEDDAH <my.meddah@gmail.com>
+ */
+export abstract class TypedFactory {
+    /**
+     * Takes a data, and model. Returns a typed object
+     * @param data: any
+     * @param model: Type<T>
+     * @returns {T | T[]}
+     */
+    static create<T>(data: any, model: Type<T>): T | T[] {
+        if (Array.isArray(data)) {
+            const response: T[] = [];
+            for (let i = 0; i < data.length; i++) {
+                response.push(new model(data[i]));
+            }
+            return response;
+        }
+        return new model(data);
+    }
+}
